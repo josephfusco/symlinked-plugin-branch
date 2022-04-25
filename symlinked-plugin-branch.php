3d0de744f8373c81e06cedf5fe259412ba9c07db
Joe Fusco <joe.fusco@wpengine.com>
2022-04-25 17:57:57 -0400

    Update license

--- symlinked-plugin-branch.php
+++ symlinked-plugin-branch.php
@@ -6,8 +6,7 @@
  * Version:        1.0.0
  * Author:         Joseph Fusco
  * Author URI:     https://josephfus.co/
- * License:        GPLv2 or later
- * License URI:    http://www.gnu.org/licenses/gpl-2.0.html
+ * License:        MIT
  * Text Domain:    symlinked-plugin-branch
  */
 
